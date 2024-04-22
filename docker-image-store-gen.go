3f5733db58fc1000e3cabeab016442b19d122d7a
Adam Olech <me@aole.ch>
2024-04-23 01:49:35 +0200

    Some initial progress

--- docker-image-store-gen.go
+++ docker-image-store-gen.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/docker/docker/image"
 )
 
 func main() {
-	fmt.Println("vim-go")
+	pathPtr := flag.String("path", "/tmp/docker-image-store/imagedb", "path to the image store")
+	flag.Parse()
 
-	image.NewFSStoreBackend("/tmp/cool")
+	imageDb, err := image.NewFSStoreBackend(*pathPtr)
+	if err != nil {
+		fmt.Printf("unable to initialize fs backend at %s: %s\n", *pathPtr, imageDb)
+		os.Exit(1)
+	}
+
+	// TODO: get the image store by creating a LayerGetRelease like so:
+	// layer.NewStoreFromOptions
+	// More info in https://github.com/moby/moby/blob/master/daemon/daemon.go#L1167
 }
