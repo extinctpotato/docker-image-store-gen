ed5bf09bb5542b83f2389d09bcef9be9665c422f
Adam Olech <me@aole.ch>
2024-04-23 02:16:46 +0200

    This will not compile but it's honest work

--- docker-image-store-gen.go
+++ docker-image-store-gen.go
@@ -4,21 +4,57 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/image"
+	"github.com/docker/docker/image/tarexport"
+	"github.com/docker/docker/layer"
+	refstore "github.com/docker/docker/reference"
 )
 
+type CustomLogger struct {
+}
+
+func (l *CustomLogger) LogImageEvent(imageID, refName string, action events.Action) {
+	fmt.Printf("Event detected on imageID %s, refName %s with action %s", imageID, refName, action)
+}
+
 func main() {
-	pathPtr := flag.String("path", "/tmp/docker-image-store/imagedb", "path to the image store")
+	pathPtr := flag.String("path", "/tmp/docker-image-store", "path to the image store")
+	tarPath := flag.String("tarpath", "/tmp/docker-image-store/test.tar", "path to the tar file to load")
 	flag.Parse()
 
-	imageDb, err := image.NewFSStoreBackend(*pathPtr)
+	layerStore, err := layer.NewStoreFromOptions(layer.StoreOptions{
+		Root:                      *pathPtr,
+		MetadataStorePathTemplate: filepath.Join(*pathPtr, "image", "%s", "layerdb"),
+		GraphDriver:               "overlay2",
+		ExperimentalEnabled:       false,
+	})
+	if err != nil {
+		fmt.Printf("unable to initialize layerStore\n")
+		os.Exit(1)
+	}
+
+	imageRoot := filepath.Join(*pathPtr, "imagedb")
+	imageDb, err := image.NewFSStoreBackend(imageRoot)
 	if err != nil {
 		fmt.Printf("unable to initialize fs backend at %s: %s\n", *pathPtr, imageDb)
 		os.Exit(1)
 	}
 
-	// TODO: get the image store by creating a LayerGetRelease like so:
-	// layer.NewStoreFromOptions
-	// More info in https://github.com/moby/moby/blob/master/daemon/daemon.go#L1167
+	refStoreLocation := filepath.Join(imageRoot, `repositories.json`)
+	rs, err := refstore.NewReferenceStore(refStoreLocation)
+	if err != nil {
+		fmt.Printf("couldn't create reference store repository: %s", err)
+		os.Exit(1)
+	}
+
+	imageStore, err := image.NewImageStore(imageDb, layerStore)
+	if err != nil {
+		fmt.Printf("couldn't create image store: %s\n", err)
+		os.Exit(1)
+	}
+
+	tarExporter := tarexport.NewTarExporter(imageStore, layerStore, rs, new(CustomLogger))
 }
